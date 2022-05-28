35e1fe3295cfea4e911d229918c6db9443c5e2ce
Michael Duisenberg <mrduise@gmail.com>
2022-05-28 16:22:04 -0700

    finished projects page

---
+++ src/container/Projects/projects.list.js
@@ -0,0 +1,17 @@
+import { images } from "../../constants";
+
+
+ const projects = {
+    projects: [
+        {
+            image: images.about01,
+            title: "Airbnb Class Project",
+            description: "This was a class project done with two other students. The goal was to create an airbnb style site for the professors summer home.",
+            demoLink: "#",
+            githubRepo: "https://github.com/MrDuise/CST-326-Project"
+        }
+    ]
+
+}
+
+export default projects;
